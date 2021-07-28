a77897076b19a5496350171f140bb67fb64d79f5
Cameron <yeec327@gmail.com>
2021-07-28 15:30:39 -0700

    Finished post component

--- src/client.js
+++ src/client.js
@@ -2,5 +2,8 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
     projectId: "d1mavt3x",
-    dataset: "production"
+    dataset: "production",
+    apiVersion: "2021-05-12",
+    token: '',
+    useCdn: false,
 })
