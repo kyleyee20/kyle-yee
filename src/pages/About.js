e216c6eba6bb4eadd4f8c68b4712c5b0e1391658
Cameron <yeec327@gmail.com>
2021-07-28 21:08:36 -0700

    Commented out author image

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,11 +32,11 @@ function About() {
     <main className="relative">
       <div className="p-10 lg:pt-48 container mx-auto relative">
         <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-          <img
+          {/* <img
             src={urlFor(author.authorImage).url()}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
             alt={author.name}
-          />
+          /> */}
           <div className="text-lg flex flex-col justify-center">
             <h1 className="text-6xl text-green-300 mb-4">
               Hey there. I'm{" "}
