1b2d120ea2d4a9665b150538e59ca32c8dce1726
Cameron <yeec327@gmail.com>
2021-08-14 23:00:34 -0700

    Added footer component to projects page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,159 +1,168 @@
 import React from "react";
+import Footer from "../components/Footer.js"
 
 function Project() {
   return (
-    <main className="projectPage">
-      <section className="projectSection">
-        <strong>
-          <h2 className="projectSectionName">Personal Projects</h2>
-        </strong>
-        <div className="grids">
-          <div className="projectCard">
-            <h3 className="projectTitle">
+    <div>
+      <main className="projectPage">
+        <section className="projectSection">
+          <strong>
+            <h2 className="projectSectionName">Personal Projects</h2>
+          </strong>
+          <div className="grids">
+            <div className="projectCard">
+              <h3 className="projectTitle">
+                <a
+                  href="https://cameron327.github.io/simon-says-game/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover-scarlett"
+                >
+                  Simon-Says Game
+                </a>
+              </h3>
               <a
                 href="https://cameron327.github.io/simon-says-game/"
                 target="_blank"
                 rel="noreferrer"
-                className="hover-scarlett"
+                className="hover-scarlett projectLink"
               >
-                Simon-Says Game
+                Check it out!
               </a>
-            </h3>
-            <a
-              href="https://cameron327.github.io/simon-says-game/"
-              target="_blank"
-              rel="noreferrer"
-              className="hover-scarlett projectLink"
-            >
-              Check it out!
-            </a>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-              tincidunt arcu, at dapibus nisl. Duis auctor dapibus accumsan.
-              Etiam neque metus, bibendum sed ligula sed, faucibus blandit
-              massa. In porttitor lobortis sapien, sollicitudin iaculis augue
-              commodo et. Fusce libero nunc, finibus in elementum id,{" "}
-            </p>
-          </div>
-          <div className="projectCard">
-            <h3 className="projectTitle">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                at tincidunt arcu, at dapibus nisl. Duis auctor dapibus
+                accumsan. Etiam neque metus, bibendum sed ligula sed, faucibus
+                blandit massa. In porttitor lobortis sapien, sollicitudin
+                iaculis augue commodo et. Fusce libero nunc, finibus in
+                elementum id,{" "}
+              </p>
+            </div>
+            <div className="projectCard">
+              <h3 className="projectTitle">
+                <a
+                  href="https://cameron327.github.io/DrumKit/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover-scarlett"
+                >
+                  Drum Simulator
+                </a>
+              </h3>
               <a
                 href="https://cameron327.github.io/DrumKit/"
                 target="_blank"
                 rel="noreferrer"
-                className="hover-scarlett"
+                className="hover-scarlett projectLink"
               >
-                Drum Simulator
+                Check it out!
               </a>
-            </h3>
-            <a
-              href="https://cameron327.github.io/DrumKit/"
-              target="_blank"
-              rel="noreferrer"
-              className="hover-scarlett projectLink"
-            >
-              Check it out!
-            </a>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-              tincidunt arcu, at dapibus nisl. Duis auctor dapibus accumsan.
-              Etiam neque metus, bibendum sed ligula sed, faucibus blandit
-              massa. In porttitor lobortis sapien, sollicitudin iaculis augue
-              commodo et. Fusce libero nunc, finibus in elementum id,{" "}
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                at tincidunt arcu, at dapibus nisl. Duis auctor dapibus
+                accumsan. Etiam neque metus, bibendum sed ligula sed, faucibus
+                blandit massa. In porttitor lobortis sapien, sollicitudin
+                iaculis augue commodo et. Fusce libero nunc, finibus in
+                elementum id,{" "}
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="projectSection">
-        <strong>
-          <h2 className="projectSectionName">Hackathon Projects</h2>
-        </strong>
-        {/* Link the githubs and the devposts? */}
-        <div className="grids">
-          <div className="projectCard">
-            <h3 className="projectTitle">
+        </section>
+        <section className="projectSection">
+          <strong>
+            <h2 className="projectSectionName">Hackathon Projects</h2>
+          </strong>
+          {/* Link the githubs and the devposts? */}
+          <div className="grids">
+            <div className="projectCard">
+              <h3 className="projectTitle">
+                <a
+                  href="https://devpost.com/software/apartment-value-estimator"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover-scarlett"
+                >
+                  Apartment-Finder Prototype
+                </a>
+              </h3>
               <a
                 href="https://devpost.com/software/apartment-value-estimator"
                 target="_blank"
                 rel="noreferrer"
-                className="hover-scarlett"
+                className="hover-scarlett projectLink"
               >
-                Apartment-Finder Prototype
+                Check it out!
               </a>
-            </h3>
-            <a
-              href="https://devpost.com/software/apartment-value-estimator"
-              target="_blank"
-              rel="noreferrer"
-              className="hover-scarlett projectLink"
-            >
-              Check it out!
-            </a>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-              tincidunt arcu, at dapibus nisl. Duis auctor dapibus accumsan.
-              Etiam neque metus, bibendum sed ligula sed, faucibus blandit
-              massa. In porttitor lobortis sapien, sollicitudin iaculis augue
-              commodo et. Fusce libero nunc, finibus in elementum id,{" "}
-            </p>
-          </div>
-          <div className="projectCard">
-            <h3 className="projectTitle">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                at tincidunt arcu, at dapibus nisl. Duis auctor dapibus
+                accumsan. Etiam neque metus, bibendum sed ligula sed, faucibus
+                blandit massa. In porttitor lobortis sapien, sollicitudin
+                iaculis augue commodo et. Fusce libero nunc, finibus in
+                elementum id,{" "}
+              </p>
+            </div>
+            <div className="projectCard">
+              <h3 className="projectTitle">
+                <a
+                  href="https://devpost.com/software/ai-on-you"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover-scarlett"
+                >
+                  Smart and Safe Security System
+                </a>
+              </h3>
               <a
                 href="https://devpost.com/software/ai-on-you"
                 target="_blank"
                 rel="noreferrer"
-                className="hover-scarlett"
+                className="hover-scarlett projectLink"
               >
-                Smart and Safe Security System
+                Check it out!
               </a>
-            </h3>
-            <a
-              href="https://devpost.com/software/ai-on-you"
-              target="_blank"
-              rel="noreferrer"
-              className="hover-scarlett projectLink"
-            >
-              Check it out!
-            </a>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-              tincidunt arcu, at dapibus nisl. Duis auctor dapibus accumsan.
-              Etiam neque metus, bibendum sed ligula sed, faucibus blandit
-              massa. In porttitor lobortis sapien, sollicitudin iaculis augue
-              commodo et. Fusce libero nunc, finibus in elementum id,{" "}
-            </p>
-          </div>
-          <div className="projectCard">
-            <h3 className="projectTitle">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                at tincidunt arcu, at dapibus nisl. Duis auctor dapibus
+                accumsan. Etiam neque metus, bibendum sed ligula sed, faucibus
+                blandit massa. In porttitor lobortis sapien, sollicitudin
+                iaculis augue commodo et. Fusce libero nunc, finibus in
+                elementum id,{" "}
+              </p>
+            </div>
+            <div className="projectCard">
+              <h3 className="projectTitle">
+                <a
+                  href="https://devpost.com/software/lurn-sfn5g3"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover-scarlett"
+                >
+                  Learning Resource Library
+                </a>
+              </h3>
               <a
                 href="https://devpost.com/software/lurn-sfn5g3"
                 target="_blank"
                 rel="noreferrer"
-                className="hover-scarlett"
+                className="hover-scarlett projectLink"
               >
-                Learning Resource Library
+                Check it out!
               </a>
-            </h3>
-            <a
-              href="https://devpost.com/software/lurn-sfn5g3"
-              target="_blank"
-              rel="noreferrer"
-              className="hover-scarlett projectLink"
-            >
-              Check it out!
-            </a>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-              tincidunt arcu, at dapibus nisl. Duis auctor dapibus accumsan.
-              Etiam neque metus, bibendum sed ligula sed, faucibus blandit
-              massa. In porttitor lobortis sapien, sollicitudin iaculis augue
-              commodo et. Fusce libero nunc, finibus in elementum id,{" "}
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                at tincidunt arcu, at dapibus nisl. Duis auctor dapibus
+                accumsan. Etiam neque metus, bibendum sed ligula sed, faucibus
+                blandit massa. In porttitor lobortis sapien, sollicitudin
+                iaculis augue commodo et. Fusce libero nunc, finibus in
+                elementum id,{" "}
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+      <Footer/>
+    </div>
   );
 }
 
