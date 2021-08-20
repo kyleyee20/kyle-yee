52d529c0a6a82006ddb68057562b3136a3a04b45
Cameron <yeec327@gmail.com>
2021-08-20 00:24:12 -0700

    Reformatted app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './pages/Home.js'
-import About from './pages/About.js'
-import Project from './pages/Project.js'
-import NavBar from './pages/NavBar.js'
-import SocialLinks from './components/SocialLinks.js'
-import ScrollToTop from './components/ScrollToTop.js'
-
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Project from "./pages/Project.js";
+import NavBar from "./pages/NavBar.js";
+import SocialLinks from "./components/SocialLinks.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar/>
-      <ScrollToTop/>
+      <NavBar />
+      <ScrollToTop />
       <Switch>
-        <Route component={Home} path='/' exact/>
-        <Route component={About} path='/about' />
-        <Route component={Project} path='/project' />
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={Project} path="/project" />
       </Switch>
-      <SocialLinks/>
+      <SocialLinks />
     </BrowserRouter>
   );
 }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -4,6 +4,7 @@ import HackathonProjectCard from "../components/HackathonProjectCard.js"
 import { PersonalProjectData, HackathonProjectData } from "../data/ProjectData.js";
 import Footer from "../components/Footer.js";
 
+// Include github source code for the projects too
 function Project() {
   return (
     <div>
@@ -31,7 +32,6 @@ function Project() {
               Hackathon Projects
             </h2>
           </strong>
-          {/* Link the githubs and the devposts? */}
           <div className="grids">
             {HackathonProjectData.map((project, index) => {
               return(
